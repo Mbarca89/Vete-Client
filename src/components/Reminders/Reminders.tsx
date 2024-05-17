4a0a0fc7b657e12b1c474aa8db178013be474bc2
Mbarca89 <mauriciobarca1989@gmail.com>
2024-05-17 19:11:08 -0300

    reminders view fix

--- src/components/Reminders/Reminders.tsx
+++ src/components/Reminders/Reminders.tsx
@@ -29,8 +29,7 @@ const Reminders = () => {
                 })), ...vaccinesResponse.data.map((reminder: reminder) => ({
                     title: reminder.name,
                     start: reminder.date,
-                    backgroundColor: "red",
-                    description: "probando"
+                    backgroundColor: "red"
                 }))])
             }
         } catch (error: any) {
@@ -46,14 +45,16 @@ const Reminders = () => {
         getReminders(dateInfo.start.toISOString())
     }
 
+    console.log(reminders)
+
     return (
-        <div className='w-100 w-md-25 h-75 custom rounded z-2 p-1'>
+        <div className='w-100 w-md-25 h-100 custom rounded z-2 p-1'>
             <div className='d-flex flex-row align-items-center justify-content-between'>
-                <h2>Recordatorios</h2>
+                <h2 className='m-0'>Recordatorios</h2>
                 <svg role="button" onClick={handleCreateReminder} width="25" height="25" viewBox="0 0 512 512" style={{ color: "#632f6b" }} xmlns="http://www.w3.org/2000/svg" className="h-full w-full"><rect width="512" height="512" x="0" y="0" rx="30" fill="transparent" stroke="transparent" strokeWidth="0" strokeOpacity="100%" paintOrder="stroke"></rect><svg width="512px" height="512px" viewBox="0 0 24 24" fill="#632f6b" x="0" y="0" role="img" style={{ display: "inline-block;vertical-align:middle" }} xmlns="http://www.w3.org/2000/svg"><g fill="#632f6b"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M12 20v-8m0 0V4m0 8h8m-8 0H4" /></g></svg></svg>
             </div>
-            <hr />
-            <div className='custom-calendar h-75 h-md-100 w-100 overflow-auto'>
+            <hr className='m-1'/>
+            <div className='custom-calendar h-100 h-md-100 w-100'>
                 <FullCalendar
                     plugins={[bootstrap5Plugin, listPlugin]}
                     initialView="listDay"
@@ -61,9 +62,10 @@ const Reminders = () => {
                     locale="esLocale"
                     buttonText={{ today: "hoy" }}
                     events={reminders}
-                    height={"100%"}
                     displayEventTime={false}
                     datesSet={(dateInfo) => handleDateChange(dateInfo)}
+                    height={'89%'}
+                    contentHeight={'100%'}
                 />
             </div>
             {show &&
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -6,8 +6,8 @@ import StockAlerts from '../../components/StockAlerts/StockAlerts'
 const Home = () => {
 
     return (
-        <div className='h-75 container flex-grow-1 d-flex flex-column flex-md-row align-items-center justify-content-center p-lg-3 p-sm-0 m-2'>
-            <div className='w-100 w-md-25 h-75 custom rounded z-2 p-1 overflow-auto'>
+        <div className='h-75 container flex-grow-1 d-flex flex-column flex-md-row align-items-center justify-content-center p-lg-3 p-sm-0 m-2 overflow-auto'>
+            <div className='w-100 w-md-25 h-75 custom rounded z-2 p-1'>
                 <Reminders />
             </div>
             <img className='w-25 custom-img' src={logo} alt="" />
