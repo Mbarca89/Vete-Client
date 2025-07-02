eeaaa0a8f5bad51b99f9ea4ca5ec673e25b09fe3
Mbarca89 <mauriciobarca1989@gmail.com>
2025-07-02 18:59:49 -0300

    iva conidtion bug fix

--- src/components/CreateBill/CreateBill.tsx
+++ src/components/CreateBill/CreateBill.tsx
@@ -161,7 +161,7 @@ const CreateBill: React.FC<CreateBillProps> = ({ updateList, saleId }) => {
                 tipoDocumento: values.type === "1" ? 80 : values.cuit ? "90" : "99",
                 documento: values.cuit || 0,
                 nombre: values.name !== "" ? values.name : "",
-                condicionFrenteIva: Number(values.condicionIvaDescripcion),
+                condicionIvaReceptorId: Number(values.condicionIvaDescripcion),
                 importeTotal: (billProducts.reduce((total, product) => total + (product.quantity * product.price), 0).toFixed(2)),
                 importeNoGravado: 0,
                 importeGravado: (billProducts.reduce((total, product) => total + (product.quantity * product.netPrice), 0).toFixed(2)),
